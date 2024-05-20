06fecb2dfbad590534194ee3b336ade1b129e3b9
MirEnthusiast <160018175+MirEnthusiast@users.noreply.github.com>
2024-05-20 16:37:57 +1200

    Add files via upload

--- car-time/network.js
+++ car-time/network.js
@@ -17,13 +17,32 @@ class NeuralNetwork{
     }
     return outputs;
   }
+
+  static mutate(network, amount = 1) {
+    network.levels.forEach(level => {
+      for (let i = 0; i < level.biases.length; i++) {
+        level.biases[i] = lerp(
+          level.biases[i],
+          Math.random() * 2 - 1,
+          amount
+        )
+      }
+      for (let i = 0; i < level.weights.length; i++) {
+        for (let j = 0; j < level.weights[i].length; j++) {
+          level.weights[i][j],
+          Math.random() * 2 - 1,
+          amount
+        }
+      }
+    });
+  }
 }
 
 class Level{
   constructor(inputCount, outputCount) {
     this.inputs = new Array(inputCount);
     this.outputs = new Array(outputCount);
-    this.biases = new Array(outputCount);
+    this.biases = new Array(outputCount).fill(0);
 
     this.weights = [];
     for (let i = 0; i < inputCount; i++) {
@@ -34,7 +53,8 @@ class Level{
   }
 
   static #randomize(level) {
-    level.weights = level.weights.map((element) => element.map((inner) => Math.random() * 2 - 1));
+    level.weights = level.weights.map((element) => element.map(() => Math.random() * 2 - 1));
+    level.biases = level.biases.map(() => Math.random() * 2 - 1)
   }
 
   static feedForward(givenInputs, level) {
